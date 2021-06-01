79de452b08f1823bfa484ebd4f312956150b0f35
Reyhan Naufal Hakim <reyhan_kim@icloud.com>
2021-06-02 01:01:46 +0700

    feat: add violation API routes

--- monolith-api/src/routes/__init__.py
+++ monolith-api/src/routes/__init__.py
@@ -1,3 +1,5 @@
 from .user import USER_BLUEPRINT
 from .cctv import CCTV_BLUEPRINT
 from .cluster import CLUSTER_BLUEPRINT
+from .violation import VIOLATION_BLUEPRINT
+from .violations import VIOLATIONS_BLUEPRINT
