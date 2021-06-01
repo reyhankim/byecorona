0fabb4c45160e399747a1fb9d01e1b55936a8dc6
Reyhan Naufal Hakim <reyhan_kim@icloud.com>
2021-06-01 22:22:06 +0700

    feat: implement cctv cluster model

--- monolith-api/src/models/cctv.py
+++ monolith-api/src/models/cctv.py
@@ -10,14 +10,16 @@ class Cctv(db.Model, BaseModel, metaclass=MetaBaseModel):
 
     __tablename__ = "cctv"
 
-    cctv_id = db.Column(db.Integer, primary_key=True)
+    cctv_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cctv_name = db.Column(db.String(300), nullable=False)
-    cctv_longitude = db.Column(db.Integer, nullable=False)
-    cctv_latitude = db.Column(db.Integer, nullable=False)
+    cctv_longitude = db.Column(db.Float, nullable=False)
+    cctv_latitude = db.Column(db.Float, nullable=False)
+    cctv_cluster_id = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, cctv_id, cctv_name, cctv_longitude, cctv_latitude):
+    def __init__(self, cctv_id, cctv_name, cctv_longitude, cctv_latitude, cctv_cluster_id):
         """ Create a new User """
         self.cctv_id = cctv_id
         self.cctv_name = cctv_name
         self.cctv_longitude = cctv_longitude
         self.cctv_latitude = cctv_latitude
+        self.cctv_cluster_id = cctv_cluster_id
