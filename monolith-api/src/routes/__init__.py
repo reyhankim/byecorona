d742cdda85bc9450a06d6dcc6d8b7180f9be4dba
Reyhan Naufal Hakim <reyhan_kim@icloud.com>
2021-06-03 00:58:12 +0700

    fix: add clusters API route

--- monolith-api/src/routes/__init__.py
+++ monolith-api/src/routes/__init__.py
@@ -1,5 +1,6 @@
 from .user import USER_BLUEPRINT
 from .cctv import CCTV_BLUEPRINT
 from .cluster import CLUSTER_BLUEPRINT
+from .clusters import CLUSTERS_BLUEPRINT
 from .violation import VIOLATION_BLUEPRINT
 from .violations import VIOLATIONS_BLUEPRINT
